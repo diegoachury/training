fab62b1615890fcbf3dfa158e7107ae735f5f3e5
diegoachury <tiran_o@hotmail.com>
2014-02-10 14:36:16 -0500

    creando repositiorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Training::Application.config.secret_key_base = '51fce7d1818d82d1aaf3c4d0c367014054ef67e04690f973b96db361dbd7c3738842809e5ac0fedd56bbc5418a5b53f749fb870d9700d017e7ceacbff0d2ffd8'
